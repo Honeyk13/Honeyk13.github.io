500803823f648e300ecab28a27b6124a9a851fe1
Hrithik Chouhan <45077877+Honeyk13@users.noreply.github.com>
2021-05-22 10:33:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -103,7 +103,7 @@ function addInTable(date, center_id, center_name, district, cap) {
 }
 
 function play() { 
-    var beepsound = new Audio('https://www.soundjay.com/buttons/sounds/beep-01a.mp3'); 
+    var beepsound = new Audio("sound/beep-04.mp3"); 
     beepsound.play(); 
 }
 
